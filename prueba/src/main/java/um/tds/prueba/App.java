894a753edbe284e8ea44eaec62c57c44103a87ae
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2021-10-10 15:35:56 +0200

    primer commit

---
+++ prueba/src/main/java/um/tds/prueba/App.java
@@ -0,0 +1,14 @@
+package um.tds.prueba;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("cyuidado");
+    }
+}
