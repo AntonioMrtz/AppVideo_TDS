58722004e2499277042d8e479eff4f806422ef86
AntonioMrtz <antonio.martinezf@um.es>
2021-10-29 20:17:05 +0200

    e

--- AppVideo/src/main/java/um/tds/AppVideo/App.java
+++ AppVideo/src/main/java/um/tds/AppVideo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World portatil!" );
+        System.out.println( "Hello World portatil123!" );
     }
 }
