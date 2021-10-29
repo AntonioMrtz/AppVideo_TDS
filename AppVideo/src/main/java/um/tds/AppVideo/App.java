10c757b5312958cace88aa00ad83174363b05f58
AntonioMrtz <antonio.martinezf@um.es>
2021-10-29 20:51:07 +0200

    e

--- AppVideo/src/main/java/um/tds/AppVideo/App.java
+++ AppVideo/src/main/java/um/tds/AppVideo/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "Hello World!si cambio portatil2" );
+        System.out.println( "Hello World!si cambio portatil2s" );
     }
 }
