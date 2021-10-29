1ac1e4ab62d91ce1c2ba50a472996aee32391ed0
AntonioMrtz <antonio.martinezf@um.es>
2021-10-29 20:36:37 +0200

    port

--- AppVideo/src/main/java/um/tds/AppVideo/App.java
+++ AppVideo/src/main/java/um/tds/AppVideo/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "Hello World!si" );
+        System.out.println( "Hello World!si cambio portatil" );
     }
 }
