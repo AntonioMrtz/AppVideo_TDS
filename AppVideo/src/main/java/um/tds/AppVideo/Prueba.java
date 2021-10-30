1ead0d53ef880a3809169079fc89cbfd14ba4561
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2021-10-30 16:06:14 +0200

    Ventana Login completada

--- AppVideo/src/main/java/um/tds/AppVideo/Prueba.java
+++ AppVideo/src/main/java/um/tds/AppVideo/Prueba.java
@@ -6,14 +6,19 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.GridBagLayout;
-import java.awt.FlowLayout;
 import javax.swing.JLabel;
 import javax.swing.JSeparator;
-import javax.swing.JButton;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import javax.swing.JTextField;
 import java.awt.Color;
+import javax.swing.Box;
+import java.awt.Component;
+import java.awt.Button;
+import java.awt.Label;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Prueba {
 
@@ -51,12 +56,14 @@ public class Prueba {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel = new JPanel();
 		frame.getContentPane().add(panel, BorderLayout.NORTH);
-		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		JLabel lblNewLabel = new JLabel("New label");
+		JLabel lblNewLabel = new JLabel("AppVideo");
+		lblNewLabel.setForeground(Color.RED);
+		lblNewLabel.setFont(new Font("Palatino Linotype", Font.BOLD, 15));
 		panel.add(lblNewLabel);
 		
 		JSeparator separator = new JSeparator();
@@ -65,81 +72,187 @@ public class Prueba {
 		JSeparator separator_1 = new JSeparator();
 		panel.add(separator_1);
 		
-		JButton btnNewButton = new JButton("New button");
-		panel.add(btnNewButton);
+		Button button = new Button("Registro");
+		panel.add(button);
 		
-		JButton btnNewButton_1 = new JButton("New button");
-		panel.add(btnNewButton_1);
+		Button button_1 = new Button("Login");
+		panel.add(button_1);
+		
+		JSeparator separator_3 = new JSeparator();
+		panel.add(separator_3);
+		
+		Button button_2 = new Button("Logout");
+		panel.add(button_2);
+		
+		JSeparator separator_4 = new JSeparator();
+		panel.add(separator_4);
 		
 		JSeparator separator_2 = new JSeparator();
 		panel.add(separator_2);
 		
-		JButton btnNewButton_2 = new JButton("New button");
-		panel.add(btnNewButton_2);
+		Button button_3 = new Button("Premium");
+		button_3.setForeground(Color.RED);
+		panel.add(button_3);
 		
 		JPanel panel_1 = new JPanel();
 		frame.getContentPane().add(panel_1, BorderLayout.CENTER);
 		panel_1.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel_2 = new JPanel();
-		panel_2.setBackground(Color.GRAY);
+		panel_2.setBackground(Color.LIGHT_GRAY);
+		panel_2.setForeground(Color.LIGHT_GRAY);
 		panel_1.add(panel_2, BorderLayout.NORTH);
-		panel_2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		GridBagLayout gbl_panel_2 = new GridBagLayout();
+		gbl_panel_2.columnWidths = new int[]{0, 0, 0, 0, 0, 0};
+		gbl_panel_2.rowHeights = new int[]{0, 0};
+		gbl_panel_2.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_2.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		panel_2.setLayout(gbl_panel_2);
 		
-		JButton btnNewButton_3 = new JButton("New button");
-		panel_2.add(btnNewButton_3);
+		Button button_4 = new Button("Explorar");
+		button_4.setForeground(Color.WHITE);
+		button_4.setBackground(Color.LIGHT_GRAY);
+		button_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		GridBagConstraints gbc_button_4 = new GridBagConstraints();
+		gbc_button_4.gridheight = 0;
+		gbc_button_4.gridx = 0;
+		gbc_button_4.gridy = 0;
+		panel_2.add(button_4, gbc_button_4);
 		
-		JButton btnNewButton_4 = new JButton(".");
-		panel_2.add(btnNewButton_4);
+		Button button_5 = new Button("Mis Listas");
+		button_5.setBackground(Color.LIGHT_GRAY);
+		button_5.setForeground(Color.WHITE);
+		GridBagConstraints gbc_button_5 = new GridBagConstraints();
+		gbc_button_5.gridx = 1;
+		gbc_button_5.gridy = 0;
+		panel_2.add(button_5, gbc_button_5);
 		
-		JButton btnNewButton_5 = new JButton("New button");
-		panel_2.add(btnNewButton_5);
+		Button button_6 = new Button("Recientes");
+		button_6.setForeground(Color.WHITE);
+		button_6.setBackground(Color.LIGHT_GRAY);
+		GridBagConstraints gbc_button_6 = new GridBagConstraints();
+		gbc_button_6.gridx = 2;
+		gbc_button_6.gridy = 0;
+		panel_2.add(button_6, gbc_button_6);
 		
-		JButton btnNewButton_6 = new JButton("New button");
-		panel_2.add(btnNewButton_6);
+		Button button_7 = new Button("Nueva Lista");
+		button_7.setBackground(Color.LIGHT_GRAY);
+		button_7.setForeground(Color.WHITE);
+		GridBagConstraints gbc_button_7 = new GridBagConstraints();
+		gbc_button_7.gridx = 3;
+		gbc_button_7.gridy = 0;
+		panel_2.add(button_7, gbc_button_7);
 		
 		JPanel panel_3 = new JPanel();
 		panel_3.setBackground(Color.GRAY);
 		panel_1.add(panel_3, BorderLayout.CENTER);
 		GridBagLayout gbl_panel_3 = new GridBagLayout();
-		gbl_panel_3.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0};
-		gbl_panel_3.rowHeights = new int[]{0, 0, 0, 0, 0, 0};
-		gbl_panel_3.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
-		gbl_panel_3.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_3.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel_3.rowHeights = new int[]{0, 0, 0, 0, 0};
+		gbl_panel_3.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_3.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		panel_3.setLayout(gbl_panel_3);
 		
-		JLabel lblNewLabel_1 = new JLabel("New label");
-		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
-		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
-		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_1.gridx = 4;
-		gbc_lblNewLabel_1.gridy = 3;
-		panel_3.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		Component verticalStrut = Box.createVerticalStrut(20);
+		GridBagConstraints gbc_verticalStrut = new GridBagConstraints();
+		gbc_verticalStrut.insets = new Insets(0, 0, 5, 5);
+		gbc_verticalStrut.gridx = 5;
+		gbc_verticalStrut.gridy = 0;
+		panel_3.add(verticalStrut, gbc_verticalStrut);
+		
+		Component verticalStrut_1 = Box.createVerticalStrut(20);
+		GridBagConstraints gbc_verticalStrut_1 = new GridBagConstraints();
+		gbc_verticalStrut_1.insets = new Insets(0, 0, 5, 0);
+		gbc_verticalStrut_1.gridx = 6;
+		gbc_verticalStrut_1.gridy = 0;
+		panel_3.add(verticalStrut_1, gbc_verticalStrut_1);
+		
+		Component verticalStrut_2 = Box.createVerticalStrut(20);
+		GridBagConstraints gbc_verticalStrut_2 = new GridBagConstraints();
+		gbc_verticalStrut_2.insets = new Insets(0, 0, 5, 5);
+		gbc_verticalStrut_2.gridx = 5;
+		gbc_verticalStrut_2.gridy = 1;
+		panel_3.add(verticalStrut_2, gbc_verticalStrut_2);
+		
+		Component horizontalStrut_1 = Box.createHorizontalStrut(20);
+		GridBagConstraints gbc_horizontalStrut_1 = new GridBagConstraints();
+		gbc_horizontalStrut_1.insets = new Insets(0, 0, 5, 5);
+		gbc_horizontalStrut_1.gridx = 0;
+		gbc_horizontalStrut_1.gridy = 2;
+		panel_3.add(horizontalStrut_1, gbc_horizontalStrut_1);
+		
+		Label label = new Label("Login");
+		label.setForeground(Color.WHITE);
+		label.setBackground(Color.GRAY);
+		GridBagConstraints gbc_label = new GridBagConstraints();
+		gbc_label.anchor = GridBagConstraints.EAST;
+		gbc_label.insets = new Insets(0, 0, 5, 5);
+		gbc_label.gridx = 5;
+		gbc_label.gridy = 2;
+		panel_3.add(label, gbc_label);
 		
 		textField = new JTextField();
 		GridBagConstraints gbc_textField = new GridBagConstraints();
 		gbc_textField.anchor = GridBagConstraints.WEST;
 		gbc_textField.insets = new Insets(0, 0, 5, 0);
-		gbc_textField.gridx = 5;
-		gbc_textField.gridy = 3;
+		gbc_textField.gridx = 6;
+		gbc_textField.gridy = 2;
 		panel_3.add(textField, gbc_textField);
 		textField.setColumns(10);
 		
-		JLabel lblNewLabel_2 = new JLabel("New label");
-		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
-		gbc_lblNewLabel_2.anchor = GridBagConstraints.EAST;
-		gbc_lblNewLabel_2.insets = new Insets(0, 0, 0, 5);
-		gbc_lblNewLabel_2.gridx = 4;
-		gbc_lblNewLabel_2.gridy = 4;
-		panel_3.add(lblNewLabel_2, gbc_lblNewLabel_2);
+		Component horizontalStrut = Box.createHorizontalStrut(20);
+		GridBagConstraints gbc_horizontalStrut = new GridBagConstraints();
+		gbc_horizontalStrut.insets = new Insets(0, 0, 0, 5);
+		gbc_horizontalStrut.gridx = 0;
+		gbc_horizontalStrut.gridy = 3;
+		panel_3.add(horizontalStrut, gbc_horizontalStrut);
+		
+		Component horizontalStrut_2 = Box.createHorizontalStrut(20);
+		GridBagConstraints gbc_horizontalStrut_2 = new GridBagConstraints();
+		gbc_horizontalStrut_2.insets = new Insets(0, 0, 0, 5);
+		gbc_horizontalStrut_2.gridx = 1;
+		gbc_horizontalStrut_2.gridy = 3;
+		panel_3.add(horizontalStrut_2, gbc_horizontalStrut_2);
+		
+		Component horizontalStrut_3 = Box.createHorizontalStrut(20);
+		GridBagConstraints gbc_horizontalStrut_3 = new GridBagConstraints();
+		gbc_horizontalStrut_3.insets = new Insets(0, 0, 0, 5);
+		gbc_horizontalStrut_3.gridx = 2;
+		gbc_horizontalStrut_3.gridy = 3;
+		panel_3.add(horizontalStrut_3, gbc_horizontalStrut_3);
+		
+		Component horizontalStrut_4 = Box.createHorizontalStrut(20);
+		GridBagConstraints gbc_horizontalStrut_4 = new GridBagConstraints();
+		gbc_horizontalStrut_4.insets = new Insets(0, 0, 0, 5);
+		gbc_horizontalStrut_4.gridx = 3;
+		gbc_horizontalStrut_4.gridy = 3;
+		panel_3.add(horizontalStrut_4, gbc_horizontalStrut_4);
+		
+		Component horizontalStrut_5 = Box.createHorizontalStrut(20);
+		GridBagConstraints gbc_horizontalStrut_5 = new GridBagConstraints();
+		gbc_horizontalStrut_5.insets = new Insets(0, 0, 0, 5);
+		gbc_horizontalStrut_5.gridx = 4;
+		gbc_horizontalStrut_5.gridy = 3;
+		panel_3.add(horizontalStrut_5, gbc_horizontalStrut_5);
+		
+		Label label_1 = new Label("Password");
+		label_1.setForeground(Color.WHITE);
+		GridBagConstraints gbc_label_1 = new GridBagConstraints();
+		gbc_label_1.anchor = GridBagConstraints.EAST;
+		gbc_label_1.insets = new Insets(0, 0, 0, 5);
+		gbc_label_1.gridx = 5;
+		gbc_label_1.gridy = 3;
+		panel_3.add(label_1, gbc_label_1);
 		
 		textField_1 = new JTextField();
 		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
 		gbc_textField_1.anchor = GridBagConstraints.WEST;
-		gbc_textField_1.gridx = 5;
-		gbc_textField_1.gridy = 4;
+		gbc_textField_1.gridx = 6;
+		gbc_textField_1.gridy = 3;
 		panel_3.add(textField_1, gbc_textField_1);
 		textField_1.setColumns(10);
 	}
-
 }
