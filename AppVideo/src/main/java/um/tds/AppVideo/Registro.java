3f8d0814d033f4ee92e387e37ced30b46366ae60
mmc98gt <mmcrealmadrid@hotmail.com>
2021-11-01 20:48:41 +0100

    Update Registro.java

--- AppVideo/src/main/java/um/tds/AppVideo/Registro.java
+++ AppVideo/src/main/java/um/tds/AppVideo/Registro.java
@@ -5,6 +5,7 @@ import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
