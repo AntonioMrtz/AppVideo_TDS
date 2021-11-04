e2deb21bba7703e0ef89abd1efb5108c705ae390
AntonioMrtz <antonio.martinezf@um.es>
2021-11-04 10:09:32 +0100

    prueba

--- AppVideo/src/main/java/um/tds/AppVideo/PanelRegistro.java
+++ AppVideo/src/main/java/um/tds/AppVideo/PanelRegistro.java
@@ -13,10 +13,6 @@ import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.FlowLayout;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.FormSpecs;
-import com.jgoodies.forms.layout.RowSpec;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
