8178404404d418a9bf76ad07f6d372af768a5fae
AntonioPC <antonio.martinezf@um.es>
2022-01-03 17:30:06 +0100

    .

--- AppVideo/src/main/java/um/tds/gui/IWindow.java
+++ AppVideo/src/main/java/um/tds/gui/IWindow.java
@@ -5,7 +5,6 @@ public interface IWindow {
 	// Metodos comunes para las diferentes pestañas del cardlayout que sirven para realizar
 	// acciones cuando son visibles y cuando dejan de serlo
 	
-	
 	void enter();
 	
 	void exit();
